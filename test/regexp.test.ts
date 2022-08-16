187d02e4b7013450eb7226a63f47c0b8f8862eca
chenym1992 <18251821296@163.com>
2022-08-16 18:12:18 +0800

    test(regexp): 相关测试用例

--- test/regexp.test.ts
+++ test/regexp.test.ts
@@ -1,4 +1,21 @@
-import { isColor, isHexColor, isRgbaColor, isRgbColor } from '../src'
+import {
+  checkPwdStrength,
+  isBase64,
+  isChinese,
+  isColor,
+  isEmail,
+  isEnglish,
+  isHexColor,
+  isIdCard,
+  isIphoneNumber,
+  isNumberPlate,
+  isRgbaColor,
+  isRgbColor,
+  isStrongPwd,
+  isTelNumber,
+  isUrl,
+  isValidIdCard
+} from '../src'
 
 describe('Regexp Funtions Api】', () => {
   test('#1 isColor', () => {
@@ -24,5 +41,67 @@ describe('Regexp Funtions Api】', () => {
     expect(isRgbaColor('RGB(255,15,12)')).toBeFalsy()
     expect(isRgbaColor('RGBA(255,15,12,0.1)')).toBeTruthy()
     expect(isRgbaColor('RGBA(255,15,12,11)')).toBeTruthy()
+  })
+
+  test('#5 isIphoneNumber', () => {
+    expect(isIphoneNumber('18951221696')).toBeTruthy()
+    expect(isIphoneNumber('+8618951221696')).toBeTruthy()
+    expect(isIphoneNumber('008618951221696')).toBeTruthy()
+  })
+
+  test('#6 isTelNumber', () => {
+    expect(isTelNumber('025-3456789')).toBeTruthy()
+    expect(isTelNumber('025-3456789-23')).toBeTruthy()
+    expect(isTelNumber('0814-34567891')).toBeTruthy()
+  })
+
+  test('#7 isIdCard', () => {
+    expect(isIdCard('430723200010014567')).toBeTruthy()
+    expect(isIdCard('43072320001001456X')).toBeTruthy()
+    expect(isIdCard('43072320001001456x')).toBeTruthy()
+    expect(isIdCard('430723200010014')).toBeTruthy()
+    expect(isValidIdCard('43072320001001456X')).toBeFalsy()
+    // 需要真实身份证号
+    // expect(isValidIdCard('460027197306180612')).toBeTruthy()
+  })
+
+  test('#8 isEmail', () => {
+    expect(isEmail('example@example.com')).toBeTruthy()
+  })
+
+  test('#9 isUrl', () => {
+    expect(isUrl('http://example.com')).toBeTruthy()
+  })
+
+  test('#10 isEnglish', () => {
+    expect(isEnglish('example')).toBeTruthy()
+  })
+
+  test('#11 isChinese', () => {
+    expect(isChinese('中国人')).toBeTruthy()
+  })
+
+  test('#12 isNumberPlate', () => {
+    expect(isNumberPlate('京A66666')).toBeTruthy()
+    expect(isNumberPlate('京AD66666')).toBeTruthy()
+  })
+
+  test('#13 isBase64', () => {
+    expect(
+      isBase64('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgA')
+    ).toBeTruthy()
+  })
+
+  test('#14 isStrongPwd', () => {
+    expect(isStrongPwd('123456aA@')).toBeTruthy()
+  })
+
+  test('#14 checkPwdStrength', () => {
+    expect(checkPwdStrength('123')).toBe(0)
+    expect(checkPwdStrength('123456')).toBe(1)
+    expect(checkPwdStrength('123456a')).toBe(2)
+    expect(checkPwdStrength('123456A')).toBe(2)
+    expect(checkPwdStrength('123456aA')).toBe(3)
+    expect(checkPwdStrength('123456aA@')).toBe(4)
   })
 })
