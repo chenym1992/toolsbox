171a9195fc46da5772acb51cb9022bb3c96fe82a
chenym1992 <18251821296@163.com>
2022-08-16 18:24:50 +0800

    chore: build dist types

--- dist/toolsbox.esm.js
+++ dist/toolsbox.esm.js
@@ -1,5 +1,5 @@
 /*!
- * Toolsbox.js v0.0.4
+ * Toolsbox.js v0.0.5
  * (c) 2014-2022 chenym1992
  * Released under the MIT License.
  */
@@ -811,6 +811,294 @@ function rgbaToHex(rgba) {
 }
 
 /**
+ * 宽松判断国内手机号
+ * @param string
+ * @returns
+ *
+ * @example
+ * isIphoneNumber('18951221696')
+ * // => true
+ * isIphoneNumber('+8618951221696')
+ * // => true
+ * isIphoneNumber('008618951221696')
+ * // => true
+ *
+ */
+function isIphoneNumber(string) {
+  return /^(?:(?:\+|00)86)?1\d{10}$/.test(string)
+}
+/**
+ * 判断座机号码（带分机号码）
+ * @param string
+ * @returns
+ *
+ * @example
+ * isTelNumber('025-3456789')
+ * // => true
+ * isTelNumber('025-3456789-23')
+ * // => true
+ * isTelNumber('0814-34567891')
+ * // => true
+ */
+
+function isTelNumber(string) {
+  return /^(0\d{2,3}-\d{7,8})(-\d{1,4})?$/.test(string)
+}
+/**
+ * 判断身份证号
+ * @param string
+ * @returns
+ *
+ * @example
+ * isIdCard('430723200010014567')
+ * // => true
+ * isIdCard('43072320001001456X')
+ * // => true
+ * isIdCard('43072320001001456x')
+ * // => true
+ * isIdCard('430723200010014')
+ * // => true
+ */
+
+function isIdCard(string) {
+  return /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/.test(string)
+}
+/**
+ * 判断身份证是否有效
+ * @param string
+ * @returns
+ *
+ * @example
+ * isValidIdCard('11010119900307221X')
+ * // => true
+ */
+
+function isValidIdCard(string) {
+  if (!isIdCard(string)) {
+    console.log('你输入的身份证长度或格式错误')
+    return false
+  } //身份证城市
+
+  var CityObject = {
+    11: '北京',
+    12: '天津',
+    13: '河北',
+    14: '山西',
+    15: '内蒙古',
+    21: '辽宁',
+    22: '吉林',
+    23: '黑龙江',
+    31: '上海',
+    32: '江苏',
+    33: '浙江',
+    34: '安徽',
+    35: '福建',
+    36: '江西',
+    37: '山东',
+    41: '河南',
+    42: '湖北',
+    43: '湖南',
+    44: '广东',
+    45: '广西',
+    46: '海南',
+    50: '重庆',
+    51: '四川',
+    52: '贵州',
+    53: '云南',
+    54: '西藏',
+    61: '陕西',
+    62: '甘肃',
+    63: '青海',
+    64: '宁夏',
+    65: '新疆',
+    71: '台湾',
+    81: '香港',
+    82: '澳门',
+    91: '国外'
+  }
+
+  if (!CityObject[string.substring(0, 2)]) {
+    console.log('你的身份证地区非法')
+    return false
+  } // 出生日期验证
+
+  var Birthday =
+      string.substring(6, 10) +
+      '-' +
+      string.substring(10, 12) +
+      '-' +
+      string.substring(12, 14),
+    date = new Date(Birthday)
+
+  if (
+    Birthday !=
+    date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate()
+  ) {
+    console.log('身份证上的出生日期非法')
+    return false
+  } // 身份证号码校验
+
+  var sum = 0
+  var weights = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2]
+  var codes = '10X98765432'
+
+  for (var i = 0; i < string.length - 1; i++) {
+    sum += +string[i] * weights[i]
+  }
+
+  var lastCode = codes[sum % 11] //计算出来的最后一位身份证号码
+
+  if (string[string.length - 1] != lastCode) {
+    console.log('你输入的身份证号非法')
+    return false
+  }
+
+  return true
+}
+/**
+ * 判断邮箱
+ * @param string
+ * @returns
+ *
+ * @example
+ * isEmail('example@example.com')
+ * // => true
+ */
+
+function isEmail(string) {
+  return /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/.test(string)
+}
+/**
+ * 判断Url
+ * @param string
+ * @returns
+ *
+ * @example
+ * isUrl('http://example.com')
+ * // => true
+ */
+
+function isUrl(string) {
+  return /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&:/~\+#]*[\w\-\@?^=%&/~\+#])?/.test(
+    string
+  )
+}
+/**
+ * 判断英文
+ * @param string
+ * @returns
+ */
+
+function isEnglish(string) {
+  return /^[a-zA-Z]+$/.test(string)
+}
+/**
+ * 判断中文
+ * @param string
+ * @returns
+ */
+
+function isChinese(string) {
+  return /^[\u4e00-\u9fa5]+$/.test(string)
+}
+/**
+ * 判断车牌号
+ * @param string
+ * @returns
+ *
+ * @example
+ * isNumberPlate('京A66666')
+ * // => true
+ * isNumberPlate('京AD66666')
+ * // => true
+ */
+
+function isNumberPlate(string) {
+  return /^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领][A-HJ-NP-Z][A-HJ-NP-Z0-9]{4,5}[A-HJ-NP-Z0-9挂学警港澳]$/.test(
+    string
+  )
+}
+/**
+ * 判断base64
+ * @param string
+ * @returns
+ *
+ * @example
+ * isBase64('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgA')
+ * // => true
+ */
+
+function isBase64(string) {
+  return /^\s*data:(?:[a-z]+\/[a-z0-9-+.]+(?:;[a-z-]+=[a-z0-9-]+)?)?(?:;base64)?,([a-z0-9!$&',()*+;=\-._~:@/?%\s]*?)\s*$/i.test(
+    string
+  )
+}
+/**
+ * 判断银行卡号(包含对公对私)
+ * @param string
+ * @returns
+ */
+
+function isBankCardNumber(string) {
+  return /^[1-9]\d{9,29}$/.test(string)
+}
+/**
+ * 强密码校验
+ * @desc 强密码校验，最少6位，包括至少1个大写字母，1个小写字母，1个数字，1个特殊字符
+ * @param string
+ * @returns
+ */
+
+function isStrongPwd(string) {
+  return /^\S*(?=\S{6,})(?=\S*\d)(?=\S*[A-Z])(?=\S*[a-z])(?=\S*[!@#$%^&*? ])\S*$/.test(
+    string
+  )
+}
+/**
+ * 检测密码强度
+ * @param string
+ * @returns
+ *
+ * @example
+ * checkPwdStrength('123')
+ * // => 0
+ * checkPwdStrength('123456')
+ * // => 1
+ * checkPwdStrength('123456a')
+ * // => 2
+ * checkPwdStrength('123456aA')
+ * // => 3
+ * checkPwdStrength('123456aA@')
+ * // => 4
+ */
+
+function checkPwdStrength(string) {
+  var strength = 0
+
+  if (string.length < 6) {
+    return strength
+  }
+
+  if (/[0-9]/.test(string)) {
+    strength++
+  }
+
+  if (/[a-z]/.test(string)) {
+    strength++
+  }
+
+  if (/[A-Z]/.test(string)) {
+    strength++
+  }
+
+  if (/[\.\|\-_!@#$%^&*? ]/.test(string)) {
+    strength++
+  }
+
+  return strength
+}
+
+/**
  * 美化秒级时长显示
  * @param duration
  * @param options
@@ -1001,6 +1289,7 @@ function stringifyQueryString(obj) {
 export {
   EventEmitter,
   byteToString,
+  checkPwdStrength,
   classof,
   dateFormat,
   debounce,
@@ -1008,23 +1297,35 @@ export {
   hexToRgb,
   hexToRgba,
   isArray,
+  isBankCardNumber,
+  isBase64,
   isBigInt,
   isBoolean,
+  isChinese,
   isColor,
   isDate,
+  isEmail,
+  isEnglish,
   isFunction,
   isHexColor,
+  isIdCard,
+  isIphoneNumber,
   isNaN,
   isNil,
   isNull,
   isNumber,
+  isNumberPlate,
   isObject,
   isRegExp,
   isRgbColor,
   isRgbaColor,
   isString,
+  isStrongPwd,
   isSymbol,
+  isTelNumber,
   isUndefined,
+  isUrl,
+  isValidIdCard,
   makeDurationPretty,
   rgbToHex,
   rgbaToHex,
--- types/index.d.ts
+++ types/index.d.ts
@@ -36,6 +36,300 @@ declare function throttle(fn: Function, delay?: number): (...args: any) => void
  */
 declare function debounce(fn: Function, delay?: number): (...args: any) => void
 
+declare type Handler<T = any> = (val: T) => void
+declare class EventEmitter {
+  private __events
+  private __once
+  /**
+   * 新增一个或多个监听函数
+   * @param events
+   * @param callback
+   * @returns
+   *
+   * @example
+   * eventObj.on('event', callback)
+   * eventObj.on('event1 event2', callback)
+   * eventObj.on({ event1: callback1, event2: callback2 })
+   */
+  on(events: string | object, callback?: Handler): void
+  /**
+   * 移除单个、多个或全部监听事件
+   * @param events
+   * @param callback
+   *
+   * @example
+   * eventObj.off('event')
+   * eventObj.off('event', callback)
+   * eventObj.off('event1 event2')
+   * eventObj.off({ event1: callback1, event2: callback2 })
+   * eventObj.off()
+   *
+   */
+  off(events?: string | object, callback?: Handler): void
+  /**
+   * 添加一个或多个单次事件
+   * @param events
+   * @param callback
+   * @returns
+   *
+   * @example
+   * eventObj.once('event', callback)
+   * eventObj.once('event1 event2', callback)
+   * eventObj.once({ event1: callback1, event2: callback2 })
+   */
+  once(events: string | object, callback?: Handler): void
+  /**
+   * 触发事件
+   * @param event
+   * @param value
+   * @returns
+   */
+  emit(event: string, value: any): void
+}
+
+/**
+ * 颜色hex转Rgb
+ * @param hexColor
+ * @returns
+ *
+ * @example
+ * hexToRgb('#FFFFFF')
+ * // => 'rgb(255,255,255)'
+ */
+declare function hexToRgb(hexColor: string): string | undefined
+/**
+ * 颜色hex转Rgba
+ * @param hexColor
+ * @returns
+ *
+ * @example
+ * hexToRgba('#FFFFFF'))
+ * // => 'rgba(255,255,255,1)'
+ */
+declare function hexToRgba(hexColor: string): string | undefined
+/**
+ * 颜色rgb转hex
+ * @param rgb
+ * @param toUpperCase
+ * @returns
+ *
+ * @example
+ * rgbToHex('rgb(0,0,15)')
+ * // => #00000F
+ * rgbToHex('rgb(0,0,15)',false)
+ * // => #00000f
+ */
+declare function rgbToHex(
+  rgb: string,
+  toUpperCase?: boolean
+): string | undefined
+/**
+ * 颜色rgba转hex
+ * @param rgba
+ * @param toUpperCase
+ * @returns
+ *
+ * @example
+ * rgbaToHex('rgba(0,0,15,0.1)')
+ * // => #00000F19
+ * rgbaToHex('rgb(0,0,15,11)')
+ * // => #00000F
+ */
+declare function rgbaToHex(
+  rgba: string,
+  toUpperCase?: boolean
+): string | undefined
+
+/**
+ * 判断hex颜色表达式
+ * @param color : string;
+ * @returns
+ *
+ * @example
+ * isHexColor('#AAA')
+ * // => true
+ * isHexColor('#AAACCC')
+ * // => true
+ * isHexColor('#AAAA')
+ * // => true
+ * isHexColor('#AAACCC75')
+ * // => true
+ */
+declare function isHexColor(color: string): boolean
+/**
+ * 判断RGB颜色表达式
+ * @param color : string;
+ * @returns
+ *
+ * @example
+ * isRgbColor('rgb(23,23,23)')
+ * // => true
+ * isRgbColor('rgba(23,23,23,0.1)')
+ * // => false
+ */
+declare function isRgbColor(color: string): boolean
+/**
+ * 判断RGBA颜色表达式
+ * @param color : string;
+ * @returns
+ *
+ * @example
+ * isRgbaColor('rgb(23,23,23)')
+ * // => false
+ * isRgbaColor('rgba(23,23,23,0.1)')
+ * // => true
+ * isRgbaColor('rgba(23,23,23,11)')
+ * // => true
+ */
+declare function isRgbaColor(color: string): boolean
+/**
+ * 判断是否是颜色表达式
+ * @param color : string;
+ * @returns
+ */
+declare function isColor(color: string): boolean
+
+/**
+ * 宽松判断国内手机号
+ * @param string
+ * @returns
+ *
+ * @example
+ * isIphoneNumber('18951221696')
+ * // => true
+ * isIphoneNumber('+8618951221696')
+ * // => true
+ * isIphoneNumber('008618951221696')
+ * // => true
+ *
+ */
+declare function isIphoneNumber(string: string): boolean
+/**
+ * 判断座机号码（带分机号码）
+ * @param string
+ * @returns
+ *
+ * @example
+ * isTelNumber('025-3456789')
+ * // => true
+ * isTelNumber('025-3456789-23')
+ * // => true
+ * isTelNumber('0814-34567891')
+ * // => true
+ */
+declare function isTelNumber(string: string): boolean
+/**
+ * 判断身份证号
+ * @param string
+ * @returns
+ *
+ * @example
+ * isIdCard('430723200010014567')
+ * // => true
+ * isIdCard('43072320001001456X')
+ * // => true
+ * isIdCard('43072320001001456x')
+ * // => true
+ * isIdCard('430723200010014')
+ * // => true
+ */
+declare function isIdCard(string: string): boolean
+/**
+ * 判断身份证是否有效
+ * @param string
+ * @returns
+ *
+ * @example
+ * isValidIdCard('11010119900307221X')
+ * // => true
+ */
+declare function isValidIdCard(string: string): boolean
+/**
+ * 判断邮箱
+ * @param string
+ * @returns
+ *
+ * @example
+ * isEmail('example@example.com')
+ * // => true
+ */
+declare function isEmail(string: string): boolean
+/**
+ * 判断Url
+ * @param string
+ * @returns
+ *
+ * @example
+ * isUrl('http://example.com')
+ * // => true
+ */
+declare function isUrl(string: string): boolean
+/**
+ * 判断英文
+ * @param string
+ * @returns
+ */
+declare function isEnglish(string: string): boolean
+/**
+ * 判断中文
+ * @param string
+ * @returns
+ */
+declare function isChinese(string: string): boolean
+/**
+ * 判断车牌号
+ * @param string
+ * @returns
+ *
+ * @example
+ * isNumberPlate('京A66666')
+ * // => true
+ * isNumberPlate('京AD66666')
+ * // => true
+ */
+declare function isNumberPlate(string: string): boolean
+/**
+ * 判断base64
+ * @param string
+ * @returns
+ *
+ * @example
+ * isBase64('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgA')
+ * // => true
+ */
+declare function isBase64(string: string): boolean
+/**
+ * 判断银行卡号(包含对公对私)
+ * @param string
+ * @returns
+ */
+declare function isBankCardNumber(string: string): boolean
+/**
+ * 强密码校验
+ * @desc 强密码校验，最少6位，包括至少1个大写字母，1个小写字母，1个数字，1个特殊字符
+ * @param string
+ * @returns
+ */
+declare function isStrongPwd(string: string): boolean
+/**
+ * 检测密码强度
+ * @param string
+ * @returns
+ *
+ * @example
+ * checkPwdStrength('123')
+ * // => 0
+ * checkPwdStrength('123456')
+ * // => 1
+ * checkPwdStrength('123456a')
+ * // => 2
+ * checkPwdStrength('123456aA')
+ * // => 3
+ * checkPwdStrength('123456aA@')
+ * // => 4
+ */
+declare function checkPwdStrength(string: string): number
+
 interface PrettyOptions {
   d: string
   h: string
@@ -56,6 +350,25 @@ declare function makeDurationPretty(
   duration: number,
   options?: PrettyOptions
 ): string | 0
+
+/**
+ * 时间格式化
+ * @param format
+ * @param time
+ * @returns
+ *
+ * @example
+ * dateFormat('YYYY')
+ * // => '2022'
+ * dateFormat('YYYY-MM-DD HH:mm')
+ * // => '2022-08-12 13:42'
+ * dateFormat('YYYYMMDDHHmm')
+ * // => '202208121342'
+ */
+declare function dateFormat(
+  time?: string | number | Date,
+  format?: string
+): string
 
 /**
  * 获取url参数
@@ -191,25 +504,48 @@ declare function isNaN(obj: any): boolean
 declare function isNil(obj: any): boolean
 
 export {
+  EventEmitter,
   byteToString,
+  checkPwdStrength,
   classof,
+  dateFormat,
   debounce,
   getUrlParams,
+  hexToRgb,
+  hexToRgba,
   isArray,
+  isBankCardNumber,
+  isBase64,
   isBigInt,
   isBoolean,
+  isChinese,
+  isColor,
   isDate,
+  isEmail,
+  isEnglish,
   isFunction,
+  isHexColor,
+  isIdCard,
+  isIphoneNumber,
   isNaN,
   isNil,
   isNull,
   isNumber,
+  isNumberPlate,
   isObject,
   isRegExp,
+  isRgbColor,
+  isRgbaColor,
   isString,
+  isStrongPwd,
   isSymbol,
+  isTelNumber,
   isUndefined,
+  isUrl,
+  isValidIdCard,
   makeDurationPretty,
+  rgbToHex,
+  rgbaToHex,
   stringToByte,
   stringifyQueryString,
   throttle
