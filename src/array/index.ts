33647152def01ad7f882eca7994132b04ad438db
chenym1992 <18251821296@163.com>
2022-08-17 15:40:15 +0800

    feat(array.ts): uniq

--- src/array/index.ts
+++ src/array/index.ts
@@ -21,3 +21,16 @@ export function sum(array: number[]) {
 export function average(array: number[]) {
   return sum(array) / array.length
 }
+
+/**
+ * 数组去重
+ * @param array
+ * @returns
+ *
+ * @example
+ * uniq([2,1,3,3])
+ * // => [2,1,3]
+ */
+export function uniq(array: Iterable<unknown> | null | undefined) {
+  return Array.from(new Set(array))
+}
