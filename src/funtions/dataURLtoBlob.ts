4328f75f8adc2e81fba0a847969bfe38eb42d0c1
chenym1992 <18251821296@163.com>
2022-09-23 10:26:31 +0800

    feat: dataURLtoBlob

---
+++ src/funtions/dataURLtoBlob.ts
@@ -0,0 +1,16 @@
+/**
+ * dataurl转Blob对象
+ * @param dataURL
+ * @returns
+ */
+export function dataURLtoBlob(dataURL: string) {
+  const arr = dataURL.split(',')
+  const mime = arr[0].match(/:(.*?);/)?.[1]
+  const bstr = atob(arr[1])
+  let n = bstr.length
+  const u8arr = new Uint8Array(n)
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n)
+  }
+  return new Blob([u8arr], { type: mime })
+}
