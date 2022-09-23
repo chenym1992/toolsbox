b4c61efee4fb97e01671c2cb92a08529d56af3b6
chenym1992 <18251821296@163.com>
2022-09-23 10:27:18 +0800

    feat(helper): compareFloatsReasonably clamp

--- src/funtions/helper.ts
+++ src/funtions/helper.ts
@@ -6,3 +6,29 @@
 export function isEvenOrOdd(num: number) {
   return num & 1
 }
+
+/**
+ * 合理范围内数据比较
+ * @param a
+ * @param b
+ * @param float
+ * @returns
+ */
+export function compareFloatsReasonably(a: number, b: number, float = 0.001) {
+  return Math.abs(a - b) < float
+}
+
+/**
+ * clamp
+ * @param val
+ * @param min
+ * @param max
+ * @returns
+ *
+ * @example
+ * clamp(1,2,3)
+ * // => 2
+ */
+export function clamp(val: number, min: number, max: number) {
+  return Math.min(max, Math.max(val, min))
+}
